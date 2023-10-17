6fe41d1aa966871f421c6a41c38afba081da78d8
japenga <105770458+japenga@users.noreply.github.com>
2023-10-17 10:41:04 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,9 @@
 //     })
 function botao(){
     this.window.location = "./index3.html";
+    const texto = document.getElementById('invitename').value;
+    localStorage.setItem('Nome da pessoa', texto);
+    window.location.href = 'index3.html';
 }
 function voltar(){
     this.window.location = "./index2.html";
@@ -12,8 +15,12 @@ function nao(t){
     var btn = t;
     btn.style.position = 'absolute';
     btn.style.bottom = gerarPosicao(10, 90);
-    btn.style.left = gerarPosicao(10, 90);
+    btn.style.right = gerarPosicao(10, 90);
 }
 function gerarPosicao(min, max){
     return (Math.random() * (max - min) + min) + "%";
+}
+const texto = localStorage.getItem('Nome da pessoa');
+if (texto){
+    document.getElementById('nomeP').textContent += `${texto}`;
 }
