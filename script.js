fbc097bac03c6ebc92c411c40c3f33e279f292ab
japenga <105770458+japenga@users.noreply.github.com>
2023-10-17 10:16:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-document.getElementById("nome")
-    addEventListener("click", function(){
-        this.window.location = "./index3.html";
-    })
-
+// document.getElementsById("nome")
+// addEventListener("click", function(){
+//         this.window.location = "./index3.html";
+//     })
+function botao(){
+    this.window.location = "./index3.html";
+}
+function voltar(){
+    this.window.location = "./index2.html";
+}
+function nao(t){
+    var btn = t;
+    btn.style.position = 'absolute';
+    btn.style.bottom = gerarPosicao(10, 90);
+    btn.style.left = gerarPosicao(10, 90);
+}
+function gerarPosicao(min, max){
+    return (Math.random() * (max - min) + min) + "%";
+}
